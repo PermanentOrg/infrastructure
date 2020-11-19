22a2694dfe76e68b069b9a91262db9d374c89ff8
Cristina Muñoz <hi@xmunoz.com>
2020-11-19 09:33:35 -0800

    Create all services

--- instances/dev.tf
+++ instances/dev.tf
@@ -12,12 +12,32 @@ provider "aws" {
     region  = "us-west-2"
 }
 
+data "aws_ami" "dev_cron" {
+  most_recent = true
+
+  filter {
+    name = "tag:Name"
+    values = ["cron-dev"]
+  }
+  owners = ["364159549467"]
+}
+
 data "aws_ami" "dev_api" {
   most_recent = true
 
   filter {
-    name = "tag:environment"
+    name = "tag:Name"
     values = ["dev"]
+  }
+  owners = ["364159549467"]
+}
+
+data "aws_ami" "dev_taskrunner" {
+  most_recent = true
+
+  filter {
+    name = "tag:Name"
+    values = ["taskrunner-dev"]
   }
   owners = ["364159549467"]
 }
@@ -39,5 +59,29 @@ resource "aws_instance" "api" {
     private_ip = "172.31.0.80"
     tags = { 
         Name = "Dev Backend"
+    }
+}
+
+resource "aws_instance" "taskrunner" {
+    ami = data.aws_ami.dev_taskrunner.id
+    instance_type = "c4.xlarge"
+    vpc_security_group_ids = [data.aws_security_group.dev_sg.id, data.aws_security_group.bitbucket_sg.id]
+    monitoring = true
+    key_name = "PermRecord"
+    private_ip = "172.31.0.79"
+    tags = {
+        Name = "Dev Taskrunner"
+    }
+}
+
+resource "aws_instance" "cron" {
+    ami = data.aws_ami.dev_cron.id
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [data.aws_security_group.dev_sg.id, data.aws_security_group.bitbucket_sg.id]
+    monitoring = true
+    key_name = "PermRecord"
+    private_ip = "172.31.0.78"
+    tags = {
+        Name = "Dev Cron"
     }
 }
