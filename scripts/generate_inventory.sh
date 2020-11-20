c75a5aa36e8b817e91cf3b56ce09af23403badd3
Cristina Muñoz <hi@xmunoz.com>
2020-11-20 15:25:06 -0800

    Escape single quotes

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -1,4 +1,4 @@
 echo "[default]" > inventory.ini
-filters="Name=tag:Name,Values=\"$1 $2\""
+filters="Name=tag:Name,Values=\'$1 $2\'"
 public_domain=`aws ec2 describe-instances --filters $filters | grep -m 1 -o "[ec0-9\-]*.us-west-2.compute.amazonaws.com"`
 echo -e "$public_domain\tansible_user=deployer\tansible_ssh_private_key_file=key" >> inventory.ini
