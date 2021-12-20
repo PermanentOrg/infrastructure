988c365cb10975f0873adea1552e1e3f3338f545
Cristina Muñoz <hi@xmunoz.com>
2021-12-20 14:00:59 -0500

    Refactor generate_inventory script

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -1,3 +1,10 @@
 echo "[default]" > inventory.ini
-public_domain=`aws ec2 describe-instances --filters 'Name=tag:Name,Values='"$1 $2"'' | grep -m 1 -o "[ec0-9\-]*.us-west-2.compute.amazonaws.com"`
-echo -e "$public_domain\tansible_user=deployer\tansible_ssh_private_key_file=key" >> inventory.ini
+public_domains=`aws ec2 describe-instances --filters 'Name=tag:type,Values='"$1 $2"'' | grep -o "[ec0-9\-]*.us-west-2.compute.amazonaws.com" | uniq`
+if [ ${#pubic_domains[@]} -eq 0 ]; then
+    echo "Something went wrong, no instances found :("
+    exit 1
+fi
+for domain in $public_domains
+do
+  echo -e "$domain\tansible_user=deployer\tansible_ssh_private_key_file=key" >> inventory.ini
+done
