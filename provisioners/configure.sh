997b4d15a172e7e4f6aff65e2e5b8c06ef9531d1
Fon E. Noel NFEBE <fenn25.fn@gmail.com>
2023-01-04 12:09:26 +0100

    WIP: Add instruction to copy ssl files
    Apache fails to start each time we do a vagrant up
    with the complain that `permanent.key` was not found
    in /etc/ssl
    
    This addeds a step to copy the ssl files from the template
    directory.
    
    Signed-off-by: Fon E. Noel NFEBE <fenn25.fn@gmail.com>

--- provisioners/configure.sh
+++ provisioners/configure.sh
@@ -92,6 +92,7 @@ chown -R www-data /var/www/
 service apache2 stop
 a2dissite 000-default
 cp $TEMPLATES_PATH/etc/apache2/apache2.conf /etc/apache2/apache2.conf
+cp $TEMPLATES_PATH/etc/ssl/* /etc/ssl
 envsubst < $TEMPLATES_PATH/etc/apache2/sites-enabled/$PERM_SUBDOMAIN.permanent.conf > /etc/apache2/sites-enabled/$PERM_SUBDOMAIN.permanent.conf
 envsubst < $TEMPLATES_PATH/etc/apache2/sites-enabled/preload.permanent.conf > /etc/apache2/sites-enabled/preload.permanent.conf
 a2enmod expires
