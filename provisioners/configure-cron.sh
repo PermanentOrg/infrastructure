6e6f8d3f79e4192b549c0d2138ead63408d1d053
Liam Lloyd <wlloyd@hmc.edu>
2024-07-10 12:09:32 -0700

    Use templates to load RDS CA certificates
    Loading the RDS CA certificates onto our EC2 images via environment
    variables posed some problems related to the preservation of whitespace
    in the certificates, so this commit updates the process to load them via
    templates instead.

--- provisioners/configure-cron.sh
+++ provisioners/configure-cron.sh
@@ -12,7 +12,6 @@ if [[ -z "$AWS_REGION" || -z "$AWS_ACCESS_KEY_ID" || -z "$AWS_ACCESS_SECRET" ]]
 fi
 
 echo $PERM_ENV  > /data/www/host.txt
-echo $AWS_RDS_CERT_BUNDLE > /etc/ca-certificates/rds-us-west-2-ca-bundle.pem
 
 # Preseed responses to New Relic installation questions
 echo newrelic-php5 newrelic-php5/application-name string $NEW_RELIC_APPLICATION_NAME | debconf-set-selections
@@ -67,6 +66,7 @@ envsubst \
   < $TEMPLATES_PATH/var/www/.aws/config \
   > /var/www/.aws/config
 chown -R www-data /var/www/
+cp "${TEMPLATES_PATH}/etc/ca-certificates/rds-us-west-2-ca-bundle.pem" /etc/ca-certificates/rds-us-west-2-ca-bundle.pem
 
 mkdir /data/tmp
 chmod 774 /data/tmp
