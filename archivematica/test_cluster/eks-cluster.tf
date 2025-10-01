d5a384d810808d260e7f154069ac632bdbaa8dda
Liam Lloyd-Tucker <liam@permanent.org>
2025-10-01 11:34:55 -0700

    Update EKS AMIs
    The AMIs we're currently using for our node groups will soon be
    unsupported, so this commit updates them to the new standard version,
    along with updating the EKS module itself to enable that.

--- archivematica/test_cluster/eks-cluster.tf
+++ archivematica/test_cluster/eks-cluster.tf
@@ -1,46 +1,49 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.0.4"
+  version = "21.2.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.32"
+  name    = local.cluster_name
+  kubernetes_version = "1.32"
 
   vpc_id                         = var.vpc_id
   subnet_ids                     = var.subnet_ids
-  cluster_endpoint_public_access = true
-  cluster_security_group_id      = var.dev_security_group_id
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::364159549467:user/liam"
-      username = "liam"
-      groups   = ["system:masters"]
+  endpoint_public_access = true
+  security_group_id      = var.dev_security_group_id
+  access_entries = {
+    liam = {
+      principal_arn     = "arn:aws:iam::364159549467:user/liam"
+      user_name         = "liam"
+      kubernetes_groups = ["eks-admins"]
     },
-    {
-      userarn  = "arn:aws:iam::364159549467:user/cecilia"
-      username = "cecilia"
-      groups   = ["system:masters"]
+    cecilia = {
+      principal_arn     = "arn:aws:iam::364159549467:user/cecilia"
+      user_name         = "cecilia"
+      kubernetes_groups = ["eks-admins"]
     }
-  ]
+  }
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-    block_device_mappings = {
-      xvda = {
-        device_name = "/dev/xvda"
-        ebs = {
-          volume_size           = 32
-          volume_type           = "gp2"
-          delete_on_termination = true
-          encrypted             = true
-        }
-      }
+  addons = {
+    coredns = {
+      most_recent                 = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts           = "OVERWRITE"
+    }
+    kube-proxy = {
+      most_recent                 = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts           = "OVERWRITE"
+    }
+    vpc-cni = {
+      most_recent                 = true
+      resolve_conflicts_on_create = "OVERWRITE"
+      resolve_conflicts           = "OVERWRITE"
     }
   }
 
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
+      ami_type = "AL2023_x86_64_STANDARD"
 
       vpc_security_group_ids = [var.dev_security_group_id, var.staging_security_group_id]
 
@@ -49,6 +52,36 @@ module "eks" {
       min_size     = 3
       max_size     = 3
       desired_size = 3
+
+      block_device_mappings = {
+        xvda = {
+          device_name = "/dev/xvda"
+          ebs = {
+            volume_size           = 32
+            volume_type           = "gp2"
+            delete_on_termination = true
+            encrypted             = true
+          }
+        }
+      }
     }
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "eks_admins_cluster_admin" {
+  metadata {
+    name = "eks-admins-cluster-admin"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind      = "Group"
+    name      = "eks-admins"
+    api_group = "rbac.authorization.k8s.io"
   }
 }
