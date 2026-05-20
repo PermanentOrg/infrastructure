587907a8fc2174eaa59fadf213924d5ac1215820
Liam Lloyd-Tucker <liam@permanent.org>
2026-05-20 09:40:16 -0700

    Add volume usage alerting to the Archivematica clusters
    We've sometimes seen Archivematica volumes run out of space, causing
    Archivematica to stop working. To prevent such outages in the future,
    this commit adds NewRelic alerting to notify us when any Archivematica
    volume usage reaches 90% of its capacity.

---
+++ archivematica/prod_cluster/newrelic_alerts.tf
@@ -0,0 +1,70 @@
+resource "newrelic_alert_policy" "pv_usage" {
+  name                = "prod-archivematica-pv-usage"
+  incident_preference = "PER_CONDITION_AND_TARGET"
+}
+
+resource "newrelic_nrql_alert_condition" "pv_usage_high" {
+  account_id  = var.new_relic_account_id
+  policy_id   = newrelic_alert_policy.pv_usage.id
+  name        = "prod-archivematica-pv-usage-high"
+  description = "Alert when a persistent volume in the prod Archivematica cluster exceeds 90% capacity"
+  enabled     = true
+
+  nrql {
+    query = "FROM K8sVolumeSample SELECT (average(fsUsedBytes) / average(fsCapacityBytes)) * 100 WHERE clusterName = '${module.eks.cluster_name}' FACET pvcName, namespaceName"
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = 90
+    threshold_duration    = 300
+    threshold_occurrences = "ALL"
+  }
+
+  fill_option        = "last_value"
+  aggregation_window = 60
+  aggregation_method = "event_flow"
+  aggregation_delay  = 120
+}
+
+resource "newrelic_notification_destination" "pv_usage_email" {
+  name = "prod-archivematica-pv-usage-email"
+  type = "EMAIL"
+
+  property {
+    key   = "email"
+    value = var.alert_email
+  }
+}
+
+resource "newrelic_notification_channel" "pv_usage_email" {
+  name           = "prod-archivematica-pv-usage-email"
+  type           = "EMAIL"
+  destination_id = newrelic_notification_destination.pv_usage_email.id
+  product        = "IINT"
+
+  property {
+    key   = "subject"
+    value = "Persistent volume usage alert: prod Archivematica cluster"
+  }
+}
+
+resource "newrelic_workflow" "pv_usage" {
+  name                  = "prod-archivematica-pv-usage"
+  muting_rules_handling = "NOTIFY_ALL_ISSUES"
+
+  issues_filter {
+    name = "policy-filter"
+    type = "FILTER"
+
+    predicate {
+      attribute = "labels.policyIds"
+      operator  = "EXACTLY_MATCHES"
+      values    = [tostring(newrelic_alert_policy.pv_usage.id)]
+    }
+  }
+
+  destination {
+    channel_id = newrelic_notification_channel.pv_usage_email.id
+  }
+}
